a8e76e84ad12b14af9c2e79ed18420e13918c4f1
c121056598@163.com <121056598@qq.com>
2017-09-17 11:32:16 +0800

    添加了登录界面、修改了展示的路径

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,24 @@
 <template>
   <div id="app">
-    <app-header></app-Header>
-    <div class="wrapper">
-      <app-sidebar></app-sidebar>
-      <div class="frame_content" v-loading.body="loading">
-        <transition @enter="onEnter" @leave="onLeave">
-          <router-view></router-view>
-        </transition>
-      </div>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import AppHeader from '@/views/include/AppHeader'
-import AppSidebar from '@/views/include/AppSidebar'
 export default {
   name: 'app',
   data () {
     return {
-      loading: false
     }
   },
   methods: {
-    onEnter () {
-      this.loading = false
-    },
-    onLeave () {
-      this.loading = true
-    }
   },
   components: {
-    AppHeader,
-    AppSidebar
   }
 }
 </script>
 <style src="./assets/css/foundation.css"></style>
 <style src="./assets/css/master.css"></style>
-<style>
-.wrapper{
-  position: relative;
-}
-  .frame_content {
-    position: absolute;
-    left: 180px;
-    right: 0;
-    width: auto;
-    transition: left .3s;
-    /* height: 100vh; */
-    height: -moz-calc(100vh - 50px);
-    height: -webkit-calc(100vh - 50px);
-    height: calc(100vh - 50px);
-    overflow: hidden;
-}  
+<style> 
 </style>
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,20 @@
-import Index from '@/views/index/Index.vue'
+import Index from '@/views/Index'
+import Cmanage from '@/views/index/Index'
+import Login from '@/views/login/Index'
+import LoginPage from '@/views/login/Login'
 
 export const routes = [{
   path: '/',
-  component: Index
+  component: Index,
+  children: [{
+    path: '/',
+    component: Cmanage
+  }]
+}, {
+  path: '/login',
+  component: Login,
+  children: [{
+    path: '/',
+    component: LoginPage 
+  }]
 }]
